6743abf23f989e73c3592d8724cc8e5edb3a5c47
{Happy <happyswaraj7667@gmail.com>
2026-03-08 23:38:21 +0530

    intro of arr

---
+++ array/intro1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    arr[0]=23;
+    arr[1]=43;
+    arr[2]=52;
+    arr[3]=74;
+    arr[4]=66;
+    for (int i=0; i<=4; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
