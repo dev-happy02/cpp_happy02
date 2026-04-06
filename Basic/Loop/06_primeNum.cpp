c181ad039a482e6e9efd9107df09429652ee590b
dev-happy02 <happyswaraj7667@gmail.com>
2026-04-06 17:41:05 +0530

    Implemented prime number check using loop

---
+++ Basic/Loop/06_primeNum.cpp
@@ -0,0 +1,21 @@
+// Check Prime Number
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, flag = 1;
+    cin >> n;
+
+    for(int i = 2; i <= n/2; i++) {
+        if(n % i == 0) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag)
+        cout << "Prime";
+    else
+        cout << "Not Prime";
+}
