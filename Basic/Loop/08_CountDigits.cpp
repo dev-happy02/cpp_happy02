2c9e2fe3d4afb8b2dacb940f89a506f2aede0a87
dev-happy02 <happyswaraj7667@gmail.com>
2026-04-06 17:47:24 +0530

    Implemented digit count using while loop

---
+++ Basic/Loop/08_CountDigits.cpp
@@ -0,0 +1,16 @@
+// Count Digits in Number
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    cin >> n;
+
+    while(n > 0) {
+        count++;
+        n /= 10;
+    }
+
+    cout << count;
+}
