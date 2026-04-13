6f534794ff48da0993ba0558e1964921f8949f26
dev-happy02 <happyswaraj7667@gmail.com>
2026-04-13 22:52:28 +0530

    Add square star pattern

---
+++ Pattern/04_square_pattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    // Outer loop for rows
+    for(int i = 0; i < n; i++) {
+        // Inner loop for columns
+        for(int j = 0; j < n; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
