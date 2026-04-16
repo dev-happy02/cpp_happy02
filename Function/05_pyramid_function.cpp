52cadb842d5841da6dc7f02d5c2a6fe1f18a7ee5
dev-happy02 <happyswaraj7667@gmail.com>
2026-04-16 22:35:37 +0530

    Add pyramid pattern using function

---
+++ Function/05_pyramid_function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void printPyramid(int n) {
+    for(int i = 1; i <= n; i++) {
+
+        // spaces
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // stars
+        for(int j = 1; j <= (2*i - 1); j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 5;
+    printPyramid(n);
+    return 0;
+}
